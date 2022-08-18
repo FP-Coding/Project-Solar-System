03a8d01be908e85fa4833b7b7f30a064e4b52899
Felipe Pinto <felipe14062000@gmail.com>
2022-08-18 20:25:43 -0300

    Correção erros de lint

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -5,12 +5,15 @@ import './CSS/PlanetCard.css';
 class PlanetCard extends Component {
   render() {
     const { planetName, planetImage } = this.props;
+    const idElement = planetImage.split('/')[3].split('.')[0].toLowerCase();
     return (
       <div data-testid="planet-card">
-        <img id={ planetName } src={ planetImage } alt={ `Planeta ${planetName}` } />
-        <p data-testid="planet-name">
-          { planetName }
-        </p>
+        <img
+          className={ idElement }
+          src={ planetImage }
+          alt={ `Planeta ${planetName}` }
+        />
+        <p data-testid="planet-name">{planetName}</p>
       </div>
     );
   }
